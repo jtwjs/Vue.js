e041459a848c221f15719f30591fbb0e0be21fad
Jung taeWoong <60641307+jtwjs@users.noreply.github.com>
2021-03-18 21:17:31 +0900

    Vue: Beginner completion

---
+++ Beginner/vue-cli/src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <AppHeader 
+      v-bind:propsdata="str"
+      v-on:renew="renewStr"></AppHeader>
+  </div>
+</template>
+
+<script>
+import AppHeader from './components/AppHeader.vue';
+
+export default {
+  data: () => {
+    /* 컴포넌트는 재사용성이 높음 
+     여러개의 컴포넌트에서 동일한 값을 참조하면 안되므로
+    (각각의 컴포넌트들이 데이터를 공유하지 않게 하기위해)
+    객체가 아닌 함수로 만들어 새 객체를 반환하도록 함*/
+      return {
+        str: 'Header',
+  }
+  },
+components: {
+  AppHeader,
+},
+methods: {
+  renewStr: function() {
+    this.str = 'hi';
+  }
+}
+}
+</script>
+
+<style>
+
+</style>
---
+++ Beginner/vue-cli/src/a.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>hello</div>  
+</template>
+
+<script>
+export default {
+  // Javascript - 인스턴스 옵션
+}
+</script>
+
+<style>
+
+</style>
