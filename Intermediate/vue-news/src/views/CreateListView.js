4d00107876437b86fa9e1f020f62cefc58b50115
Jung taeWoong <60641307+jtwjs@users.noreply.github.com>
2021-04-03 13:07:25 +0900

    Vue: router navigation guard

--- Intermediate/vue-news/src/views/CreateListView.js
+++ Intermediate/vue-news/src/views/CreateListView.js
@@ -1,20 +1,23 @@
 import ListView from './ListView';
 import bus from '../utils/bus';
+import { store } from '../store/index';
 
 export default function createListView(name) {
   return {
     // 재사용할 인스턴스(컴포넌트) 옵션들이 들어갈 자리
     name,
-    created(){
-
+    beforeRouteEnter : (to, from, next) => {
       bus.$emit('start:spinner');
-      
-      this.$store.dispatch('FETCH_LIST', this.$route.name)
-      .then(() => {
+      // #1
+        store.dispatch('FETCH_LIST', to.name)
+        .then(() => { // #5
+          next();
+        })
+        .catch(err => console.log(err));
+     },
+     mounted() {
         bus.$emit('end:spinner');
-      })
-      .catch(err => console.log(err));
-    },
+     },
     render(createElement) {
       return createElement(ListView);
     }
