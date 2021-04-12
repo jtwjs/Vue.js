b7a35645cadf082df28c44ef199caefd821f9c1d
Jung taeWoong <60641307+jtwjs@users.noreply.github.com>
2021-04-12 23:09:17 +0900

    Vue: axios.interceptors, token

---
+++ Expert/vue-endgame/vue-til-client/src/api/common/interceptors.js
@@ -0,0 +1,29 @@
+import store from '@/store/index';
+
+export function setInterceptors(instance) {
+  // Add a request interceptor
+  instance.interceptors.request.use(
+    function(config) {
+      // Do something before request is sent
+      console.log(config);
+      config.headers.Authorization = store.state.token;
+      return config;
+    },
+    function(error) {
+      // Do something with request error
+      return Promise.reject(error);
+    },
+  );
+
+  // Add a response interceptor
+  instance.interceptors.response.use(
+    function(response) {
+      return response;
+    },
+    function(error) {
+      return Promise.reject(error);
+    },
+  );
+
+  return instance;
+}
