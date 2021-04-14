ff2df874e404e25aca411a41a61e51b767fdf025
Jung taeWoong <60641307+jtwjs@users.noreply.github.com>
2021-04-14 22:14:55 +0900

    Vue: query Data API & add loading spinners

--- Expert/vue-endgame/vue-til-client/src/views/MainPage.vue
+++ Expert/vue-endgame/vue-til-client/src/views/MainPage.vue
@@ -1,11 +1,51 @@
 <template>
   <div>
-    Main
+    <div class="main list-container contents">
+      <h1 class="page-header">Today I Learned</h1>
+      <LoadingSpinner v-if="isLoading" />
+      <ul v-else>
+        <PostListItem
+          v-for="postItem in postItems"
+          :key="postItem._id"
+          :postItem="postItem"
+        />
+      </ul>
+    </div>
   </div>
 </template>
 
 <script>
-export default {};
+import PostListItem from '@/components/posts/PostListItem';
+import LoadingSpinner from '@/components/common/LoadingSpinner';
+import { fetchPosts } from '@/api/index';
+
+export default {
+  components: {
+    PostListItem,
+    LoadingSpinner,
+  },
+  data() {
+    return {
+      postItems: [],
+      isLoading: false,
+    };
+  },
+  methods: {
+    async fetchData() {
+      this.isLoading = true;
+      const {
+        data: { posts },
+      } = await fetchPosts();
+      console.log(posts);
+      this.isLoading = false;
+
+      this.postItems = posts;
+    },
+  },
+  created() {
+    this.fetchData();
+  },
+};
 </script>
 
 <style></style>
